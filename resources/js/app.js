12153f43f292306c70b99c4adcd27e14f565a3b8
Adrian Kokot <adrian.kokot@bbraun.com>
2020-08-03 13:56:44 +0200

    Add vue-router, bootstrap-vue, fullcalendar and laravel auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+import VueRouter from 'vue-router';
+
+window.Vue = require('vue');
+
+Vue.use(VueRouter);
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+const app = new Vue({
+    el: '#app',
+});
