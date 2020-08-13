9677906acd392e72e5e1c9bb83bd709d4a4e2264
Adrian Kokot <adrian.kokot@bbraun.com>
2020-08-13 08:16:45 +0200

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,9 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -14,6 +14,15 @@
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
 
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+
+  <meta name="theme-color" content="#00B580">
+  <meta name="msapplication-navbutton-color" content="#00B580">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#00B580">
 </head>
 
 <body>
